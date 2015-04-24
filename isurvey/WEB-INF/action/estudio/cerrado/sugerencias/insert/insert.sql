ba2582714a02cc3f9bb95d30c6694e4a23abfd1a
Gary Bustillos <gary.bustillos@addsolutions.com.ve>
2015-04-24 18:19:14 -0430

    agregado update estatus en visor estudio cerrado

---
+++ isurvey/WEB-INF/action/estudio/cerrado/sugerencias/insert/insert.sql
@@ -0,0 +1,4 @@
+insert into ajvieira_isurvey_app.sugerencia
+	(id_instrumento, sugerencia)
+values
+	(${fld:id_instrumento}, ${fld:sugerencia})
--- isurvey/WEB-INF/source/estudio/cerrado/MostrarVisor.java
+++ isurvey/WEB-INF/source/estudio/cerrado/MostrarVisor.java
@@ -21,20 +21,25 @@ public class MostrarVisor extends GenericTransaction {
 
     public int service(Recordset inputParams) throws Throwable{
         this.getDb().beginTrans();
+    	String red = "<img src=\"${def:context}/images/reddot.png\" " +
+		"title=\"Incompleta\" class=\"tool\" width=\"24\" height=\"24\">";
+    	String green = "<img src=\"${def:context}/images/greendot.png\" " +
+		"title=\"Completa\" class=\"tool\" width=\"24\" height=\"24\">";
         Enumeration names = this.getRequest().getParameterNames();
         Map parametros = this.getRequest().getParameterMap();
         String token = ((String[]) parametros.get("token"))[0];
         Recordset participante = getParticipante(token);
-        Recordset listas = getListas(token);
         participante.top();
-        listas.top();
         Recordset visor = new Recordset();
         visor.append("estudio", java.sql.Types.VARCHAR);
         visor.append("instrumento", java.sql.Types.VARCHAR);
         visor.append("id_instrumento", java.sql.Types.VARCHAR);
         visor.append("token", java.sql.Types.VARCHAR);
-        while (listas.next()){
-        	Recordset info = getInfoVisor(listas.getString("id_lista_participantes"), token);
+        visor.append("estatus", java.sql.Types.VARCHAR);
+        
+        updateStatus(token);
+        
+        	Recordset info = getInfoVisor(token);
         	info.top();
         	while (info.next()){
         		visor.addNew();
@@ -42,10 +47,13 @@ public class MostrarVisor extends GenericTransaction {
 	        	visor.setValue("instrumento", info.getString("instrumento"));
 	        	visor.setValue("id_instrumento", info.getString("id_instrumento"));
 	        	visor.setValue("token", info.getString("token"));
-	        	System.out.println("visor.estudio: " + visor.getValue("estudio"));
-	        	System.out.println("visor.instrumento: " + visor.getValue("instrumento"));
+	        	if (info.getValue("estatus").equals("Incompleta")){
+	        		visor.setValue("estatus", red);
+	        	}
+	        	else{
+	        		visor.setValue("estatus", green);
+	        	}
         	}
-        }
         this.publish("visor", visor);
        // this.getSession().setAttribute("visor", visor);
 
@@ -80,42 +88,68 @@ public class MostrarVisor extends GenericTransaction {
     	return participante;
     }
     
-    Recordset getListas (String token) throws Throwable{
-    	String query = "select lista_participantes.* " +
-    			"from ajvieira_isurvey_app.lista_participantes, " +
-    			"ajvieira_isurvey_app.empresa, " +
-    			"ajvieira_isurvey_app.int_lista_participantes_estudio, " +
-    			"ajvieira_isurvey_app.estudio, " +
-    			"ajvieira_isurvey_app.int_participante_instrumento, " +
-    			"ajvieira_isurvey_app.participante, " +
-    			"ajvieira_isurvey_app.instrumento " +
+    Recordset getInfoVisor (String token) throws Throwable{
+    	String query = "select estudio.nombre_estudio as estudio, instrumento.nombre as instrumento, " +
+    			"instrumento.id_instrumento, '"+token+"' as token, int_participante_instrumento.estatus " +
+    			"from " +
+    			"ajvieira_isurvey_app.estudio, ajvieira_isurvey_app.instrumento, " +
+    			"ajvieira_isurvey_app.int_participante_instrumento " +
     			"where " +
-    			"participante.id_participante = int_participante_instrumento.id_participante " +
-    			"and instrumento.id_instrumento = int_participante_instrumento.id_instrumento " +
-    			"and instrumento.id_estudio = estudio.id_estudio " +
-    			"and estudio.id_empresa = empresa.id_empresa " +
-    			"and int_lista_participantes_estudio.id_lista_participantes = lista_participantes.id_lista_participantes " +
-    			"and int_lista_participantes_estudio.id_estudio = estudio.id_estudio " +
-    			"and lista_participantes.id_empresa = empresa.id_empresa " +
-    			"and participante.id_empresa = empresa.id_empresa " +
-    			"and int_participante_instrumento.token_participante = '" + token + "'";
-    	Recordset listas = this.getDb().get(query);
-    	return listas;
-    }
-    
-    Recordset getInfoVisor (String idLista, String token) throws Throwable{
-    	String query = "select estudio.nombre_estudio as estudio, instrumento.nombre as instrumento, instrumento.id_instrumento, '" + token + "' as token " +
-    			"from ajvieira_isurvey_app.estudio, " +
-    			"ajvieira_isurvey_app.instrumento, " +
-    			"ajvieira_isurvey_app.int_lista_participantes_estudio, " +
-    			"ajvieira_isurvey_app.lista_participantes " +
-    			"where " +
-    			"int_lista_participantes_estudio.id_lista_participantes = lista_participantes.id_lista_participantes " +
-    			"and int_lista_participantes_estudio.id_estudio = estudio.id_estudio " +
-    			"and instrumento.id_estudio = estudio.id_estudio " +
-    			"and lista_participantes.id_lista_participantes = " + idLista + " " +
-    			"order by estudio";
+    			"estudio.id_estudio = instrumento.id_estudio " +
+    			"and int_participante_instrumento.id_instrumento = instrumento.id_instrumento " +
+    			"and int_participante_instrumento.id_participante = (select id_participante from  " +
+    			"ajvieira_isurvey_app.int_participante_instrumento " +
+    			"where token_participante = '"+token+"') " +
+    			"order by estudio, instrumento";
     	Recordset info = this.getDb().get(query);
     	return info;
+    }
+    
+    Recordset getNombresColumnas (String nombreTabla) throws Throwable{
+    	String sql = "SELECT `COLUMN_NAME` " +
+    			"FROM `INFORMATION_SCHEMA`.`COLUMNS` " +
+    			"WHERE `TABLE_SCHEMA`='ajvieira_isurvey_lime' " +
+    			"AND `TABLE_NAME`='"+nombreTabla+"';";
+    	return this.getDb().get(sql);
+    }
+    
+    void setEstatus (String token, String estatus) throws Throwable{
+    	String sql = "update ajvieira_isurvey_app.int_participante_instrumento set estatus = '" + estatus + "' " +
+    			"where token_participante = '" + token + "'";
+    	this.getDb().exec(sql);
+    }
+    
+    void updateStatus (String token) throws Throwable{
+    	String estatus = "Completa";
+    	String query = "select instrumento.id_instrumento " +
+		"from " +
+		"ajvieira_isurvey_app.estudio, ajvieira_isurvey_app.instrumento, " +
+		"ajvieira_isurvey_app.int_participante_instrumento " +
+		"where " +
+		"estudio.id_estudio = instrumento.id_estudio " +
+		"and int_participante_instrumento.id_instrumento = instrumento.id_instrumento " +
+		"and int_participante_instrumento.id_participante = (select id_participante from  " +
+		"ajvieira_isurvey_app.int_participante_instrumento " +
+		"where token_participante = '"+token+"') ";
+    	Recordset instrumentos = this.getDb().get(query);
+    	instrumentos.top();
+    	while (instrumentos.next()){
+    		String sql = "select * from ajvieira_isurvey_lime.survey_" + instrumentos.getString("id_instrumento") + " " +
+    				"where token = '" + token + "'";
+    		Recordset respuestas = this.getDb().get(sql);
+    		respuestas.top();
+    		Recordset columnas = getNombresColumnas("survey_" + instrumentos.getString("id_instrumento"));
+    		columnas.top();
+    		while (respuestas.next()){
+	    		while (columnas.next()){
+	    			String column = columnas.getString("column_name");
+	    			column = column.toLowerCase();
+	    			if (respuestas.getString(column) == null){
+	    				estatus = "Incompleta";
+	    			}
+	    		}
+    		}
+    		setEstatus(token, estatus);
+    	}
     }
 }
