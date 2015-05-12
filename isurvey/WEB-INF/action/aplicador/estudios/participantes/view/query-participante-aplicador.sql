66997759df1c22a933e2b7759397fbf251066dc3
Gary Bustillos <gary.bustillos@addsolutions.com.ve>
2015-05-12 09:54:04 -0430

    Arreglada la lista de participantes por estudio de administrador

--- isurvey/WEB-INF/action/aplicador/estudios/participantes/view/query-participante-aplicador.sql
+++ isurvey/WEB-INF/action/aplicador/estudios/participantes/view/query-participante-aplicador.sql
@@ -1,17 +1,22 @@
 select
-	distinct participante.*
+	distinct participante.id_participante, participante.nombre_participante, participante.apellido_participante,
+	int_participante_instrumento.token_participante
 from
 	ajvieira_isurvey_app.participante, ajvieira_isurvey_app.int_participante_lista_participantes,
 	ajvieira_isurvey_app.lista_participantes, ajvieira_isurvey_app.int_lista_participantes_estudio,
-	ajvieira_isurvey_app.estudio, ajvieira_isurvey_app.int_estudio_usuario
+	ajvieira_isurvey_app.estudio, ajvieira_isurvey_app.int_estudio_usuario, ajvieira_isurvey_app.int_participante_instrumento,
+	ajvieira_isurvey_app.instrumento
 where
-	int_estudio_usuario.id_usuario in (select ajvieira_isurvey_security.s_user.user_id from ajvieira_isurvey_security.s_user where ajvieira_isurvey_security.s_user.userlogin = '${def:user}')
+	int_estudio_usuario.id_usuario = (select ajvieira_isurvey_security.s_user.user_id from ajvieira_isurvey_security.s_user where ajvieira_isurvey_security.s_user.userlogin = '${def:user}')
 	and int_estudio_usuario.id_estudio = estudio.id_estudio
 	and int_lista_participantes_estudio.id_estudio = estudio.id_estudio
 	and int_lista_participantes_estudio.id_lista_participantes = lista_participantes.id_lista_participantes
 	and int_participante_lista_participantes.id_lista_participantes = lista_participantes.id_lista_participantes
 	and int_participante_lista_participantes.id_participante = participante.id_participante
-	and participante.id_empresa in (select id_empresa from ajvieira_isurvey_security.s_user where userlogin = '${def:user}')
+	and participante.id_empresa = (select id_empresa from ajvieira_isurvey_security.s_user where userlogin = '${def:user}')
 	and estudio.id_estudio = ${fld:id_estudio}
+	and int_participante_instrumento.id_participante = participante.id_participante
+	and instrumento.id_instrumento = int_participante_instrumento.id_instrumento
+	and instrumento.id_estudio = estudio.id_estudio
 order by
 	id_participante
